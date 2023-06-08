0c4f561f49dbbe8656bd9951b170c89d2e39044b
xerpa43 <angngima50@gmail.com>
2023-06-08 21:07:18 +0545

    added the price and functionality to each rows

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 let rows = 1;
-const memorySelect = document.getElementById("Memory");
-const storageSelect = document.getElementById("Storage");
-const priceElement = document.querySelector(".each.price");
-const totalPriceElement = document.getElementById("totalPrice");
+let chooosenMemory = 0;
+let chooosenStorage = 0;
+let currencySelect = document.querySelectorAll(".currency");
+let memorySelect = document.querySelectorAll(".Memory");
+let priceElement = document.querySelector(".each.price");
+let storageSelect = document.querySelectorAll(".Storage");
+let service = document.querySelectorAll(".service");
+let number = document.querySelectorAll(".number");
+let jpt = document.querySelector(".total");
 const conversionRate = 118.5; // Conversion rate from USD to Nepali Rupees
-const currencySelect = document.getElementById("currency");
-memorySelect.addEventListener("change", calculateTotalPrice);
-storageSelect.addEventListener("change", calculateTotalPrice);
+
+changedRow();
+
 const addTable = document.getElementsByClassName("Add");
-const service = document.getElementById("service");
 const remove = document.getElementsByClassName("Delete");
-console.log(service);
-const number = document.getElementById("number");
-function calculateTotalPrice() {
-  const selectedMemory = parseInt(memorySelect.value);
-  const selectedStorage = parseInt(storageSelect.value);
-  const selectedService = parseInt(service.value);
-  const selectedNumber = parseInt(number.value);
+function calculateTotalPrice(row) {
+  const selectedMemory = parseInt(memorySelect[row].value);
+  const selectedStorage = parseInt(storageSelect[row].value);
+  const selectedService = parseInt(service[row].value);
+  const selectedNumber = parseInt(number[row].value);
   const pricePerUnit = parseFloat(priceElement.innerText);
 
   const totalPrice =
@@ -25,31 +27,48 @@ function calculateTotalPrice() {
     selectedStorage *
     selectedService *
     selectedNumber;
+  // console.log(pricePerUnit, selectedMemory, selectedStorage, selectedService, selectedNumber);
 
   const totalPriceUSD = totalPrice.toFixed(2);
   const totalPriceNPR = (totalPrice * conversionRate).toFixed(2);
-  currencySelect.innerText = `USD: ${totalPriceUSD} | Nepali: ${totalPriceNPR}`;
+  currencySelect[
+    row
+  ].innerText = `USD: ${totalPriceUSD} | Nepali: ${totalPriceNPR}`;
+
+  console.log(chooosenMemory + " ");
+
+  jpt.innerHTML = `
+    <table class="totalclasses">
+      <tr>
+        <th> </th>
+        <th> </th> <th> </th> <th> </th> <th> </th>
+        <th> </th>
+      </tr>
+      <tr>
+        <td>Total Memory used</td>
+        <td>Total Storage used</td>
+        <td>Total Price:</td>
+      </tr>
+    </table>
+  `;
 }
 
-
-
 remove[0].addEventListener("click", () => {
-  rows=rows-1;
-  console.log(rows);
-  const table = document.getElementById("rows").parentElement;
-   table.removeChild(table.lastChild);
+  if (rows >= 0) {
+    rows--;
+    const table = document.getElementById("rows").parentElement;
+    table.removeChild(table.lastChild);
+    checkForChanges();
+  }
 });
 
-
-
 addTable[0].addEventListener("click", () => {
-  rows=rows+1;
-  console.log(rows)
+  rows = rows + 1;
   const table = document.getElementById("rows").parentElement;
   const newRow = document.createElement("tr");
   newRow.id = "rows";
   newRow.innerHTML = ` <td>
-    <select name="service" id="service">
+    <select class="service" id="service">
       <option value="0" selected>Select an option</option>
       <option value="1">VPS Based server Solution -1</option>
       <option value="1">VPS Based server Solution -2</option>
@@ -57,15 +76,15 @@ addTable[0].addEventListener("click", () => {
     </select>
   </td>
   <td>
-    <select name="number" id="number">
-      <option value="1" selected>SIN</option>
+    <select class="number" id="number">
+      <option value="0" selected>SIN</option>
       <option value="1">Nutanx HCI - NX8155N-G8</option>
       <option value="1">Nutanx HCI - NX8155N-G8</option>
       <option value="1">Nutanx HCI - NX8155N-G8</option>
     </select>
   </td>
   <td>
-    <select name="Memory" id="Memory">
+    <select class="Memory" id="Memory">
       <option value="0" selected>Select Total Memory</option>
       <option value="2">2</option>
       <option value="4">4</option>
@@ -74,7 +93,7 @@ addTable[0].addEventListener("click", () => {
   </td>
   <td>
     <select class="Storage" id="Storage">
-      <option value="1" selected>Storage</option>
+      <option value="0" selected>Storage</option>
       <option value="25">25</option>
       <option value="50">50</option>
       <option value="80">80</option>
@@ -83,9 +102,36 @@ addTable[0].addEventListener("click", () => {
   <td>
     <div class="each price">233$</div>
   </td>
-  <td id="currency">USD: Nepali</td>`;
+  <td class="currency">USD: Nepali</td>`;
 
   table.appendChild(newRow);
-  calculateTotalPrice();
+  checkForChanges();
 });
 
+function checkForChanges() {
+  currencySelect = document.querySelectorAll(".currency");
+  memorySelect = document.querySelectorAll(".Memory");
+  // priceElement = document.querySelectorAll(".each.price");
+  storageSelect = document.querySelectorAll(".Storage");
+  service = document.querySelectorAll(".service");
+  number = document.querySelectorAll(".number");
+  changedRow();
+}
+function changedRow() {
+  for (let i = 0; i < memorySelect.length; i++) {
+    memorySelect[i].addEventListener("change", () => {
+      calculateTotalPrice(i);
+    });
+    storageSelect[i].addEventListener("change", () => {
+      calculateTotalPrice(i);
+    });
+    service[i].addEventListener("change", () => {
+      calculateTotalPrice(i);
+    });
+    number[i].addEventListener("change", () => {
+      calculateTotalPrice(i);
+    });
+  }
+}
+
+console.log(chooosenMemory);
